7ea50267f1d71f696adae9a49afafa19290584c1
Zorba11 <AGeorge@gannett.com>
2019-10-03 17:49:54 -0400

    removed template codes, added  img for land page

---
+++ client/src/components/layout/landing.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const landing = () => {
+  return (
+    <div>
+      <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">TheNetwork</h1>
+          <p className="lead">
+           Create, Collaborate, Grow
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">Sign Up</a>
+            <a href="login.html" className="btn btn-light">Login</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    </div>
+  )
+}
+
+export default landing
